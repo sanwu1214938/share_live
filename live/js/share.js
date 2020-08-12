97a95b62400b6620c77cb4a145914ce5a809980d
zhangdongming <351214938@qq.com>
2020-08-12 17:05:05 +0800

    ....

--- live/js/share.js
+++ live/js/share.js
@@ -237,43 +237,20 @@ var app = new Vue({
 			var _this = this;
 			var player = new Aliplayer({
 				id: 'xinyu_video',
+				height: '100%',
+				width: '100%',
 				source: videourl,
 				autoplay: true,
 				isLive: true,
-				height: '100%',
-				width: '100%',
-				playsinline: true,
 				useH5Prism: true,
 				useFlashPrism: false,
-				cover: './img/bg_download@2x.png',
-				//prismplayer 2.0.1版本支持的属性，主要用户实现在android 微信上的同层播放
+				playsinline: true,
 				x5_type: 'h5', //通过 video 属性 “x5-video-player-type” 声明启用同层H5播放器，支持的值：h5 https://x5.tencent.com/tbs/guide/video.html
 				x5_fullscreen: true, //通过 video 属性 “x5-video-player-fullscreen” 声明视频播放时是否进入到 TBS 的全屏模式，支持的值：true
-				skinLayout: [{
-						"name": "bigPlayButton",
-						"align": "blabs",
-						"x": 30,
-						"y": 80
-					},
-					{
-						"name": "errorDisplay",
-						"align": "tlabs",
-						"x": 0,
-						"y": 0
-					},
-					{
-						"name": "infoDisplay"
-					},
-					{
-						"name": "controlBar",
-						"align": "blabs",
-						"x": 0,
-						"y": 0,
-						"children": []
-					}
-				]
+				cover: './img/bg_download@2x.png',
 			}, function(player) {
 				console.log("The player is created");
+				$('#xinyu_video video').attr('poster', './img/bg_download@2x.png');
 			});
 
 			// 同层播放设置
